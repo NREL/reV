62d7645719038d20b19eb79fe37f1b268ff9a181
Michael Rossol <michael.rossol@nrel.gov>
2019-06-07 10:57:36 -0600

    streamline version handling

--- reV/__init__.py
+++ reV/__init__.py
@@ -3,10 +3,10 @@ The Renewable Energy Potential Model (v2)
 """
 from __future__ import print_function, division, absolute_import
 import os
+from reV2.version import __version__
 
 __author__ = """Galen Maclaurin"""
 __email__ = "galen.maclaruin@nrel.gov"
-__version__ = "2.0.0"
 
 REVDIR = os.path.dirname(os.path.realpath(__file__))
 TESTDATADIR = os.path.join(os.path.dirname(REVDIR), 'tests', 'data')
