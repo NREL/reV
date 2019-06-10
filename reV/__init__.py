e6879d1f0ad95b3781bfaecb6e49f6afd9fdaed3
gbuster <grant.buster@nrel.gov>
2019-06-10 08:35:41 -0600

    bug fix on reV version import

--- reV/__init__.py
+++ reV/__init__.py
@@ -3,7 +3,7 @@ The Renewable Energy Potential Model (v2)
 """
 from __future__ import print_function, division, absolute_import
 import os
-from reV2.version import __version__
+from reV.version import __version__
 
 __author__ = """Galen Maclaurin"""
 __email__ = "galen.maclaruin@nrel.gov"
