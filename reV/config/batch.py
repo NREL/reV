6a7d2cc8679822f0f809409b4d591c553b3916ca
gbuster <grant.buster@nrel.gov>
2019-06-11 16:20:00 -0600

    updated readme and batch config preflight check

--- reV/config/batch.py
+++ reV/config/batch.py
@@ -4,6 +4,7 @@ Created on Mon Jun 10 13:49:53 2019
 
 @author: gbuster
 """
+import os
 from reV.utilities.exceptions import ConfigError
 from reV.config.base_config import BaseConfig
 
@@ -24,11 +25,45 @@ class BatchConfig(BaseConfig):
                               'but received a "{}".'.format(type(config)))
 
         super().__init__(config)
+        self._pre_flight()
 
     def _pre_flight(self):
         """Run pre-flight checks on the config."""
+
+        self._check_pipeline()
+        self._check_sets()
+
+    def _check_pipeline(self):
+        """Check the pipeline config file in the batch config."""
+
         if 'pipeline_config' not in self:
             raise ConfigError('Batch config needs "pipeline_config" arg!')
+
+        if not os.path.exists(self['pipeline_config']):
+            raise ConfigError('Could not find the pipeline config file: {}'
+                              .format(self['pipeline_config']))
+
+    def _check_sets(self):
+        """Check the batch sets for required inputs and valid files."""
+
+        if 'sets' not in self:
+            raise ConfigError('Batch config needs "sets" arg!')
+
+        if not isinstance(self['sets'], list):
+            raise ConfigError('Batch config needs "sets" arg to be a list!')
+
+        for s in self['sets']:
+            if not isinstance(s, dict):
+                raise ConfigError('Batch sets must be dictionaries.')
+            if 'args' not in s:
+                raise ConfigError('All batch sets must have "args" key.')
+            if 'files' not in s:
+                raise ConfigError('All batch sets must have "files" key.')
+
+            for fpath in s['files']:
+                if not os.path.exists(fpath):
+                    raise ConfigError('Could not find file to modify in batch '
+                                      'jobs: {}'.format(fpath))
 
     @property
     def config_pipeline(self):
