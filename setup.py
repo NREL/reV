bfc5b8e8c464b2b4311ffc4d9ca0b1b81d4c5271
Michael Rossol <michael.rossol@nrel.gov>
2019-06-20 09:34:39 -0600

    update requirements

--- setup.py
+++ setup.py
@@ -101,7 +101,8 @@ setup(
         "Programming Language :: Python :: 3.7",
     ],
     test_suite="tests",
-    install_requires=["click", "h5py", "numpy", "pandas", "psutil"],
+    install_requires=["click", "h5py", "numpy", "pandas", "psutil",
+                      "rasterio"],
     extras_require={
         "test": test_requires,
         "dev": test_requires + ["pypandoc", "flake8", "pre-commit", "pylint"],
