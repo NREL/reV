541a64b2e90862ccacb49e9dcc0b481ea9117602
gbuster <grant.buster@nrel.gov>
2019-07-03 10:02:40 -0600

    Added generic data layer aggregation methods. Added Aggregation file handler to handle the context managers for excl/gen/techmap and any number of data layer geotiffs.

--- reV/supply_curve/aggregation.py
+++ reV/supply_curve/aggregation.py
@@ -12,6 +12,7 @@ import numpy as np
 import pandas as pd
 from warnings import warn
 import logging
+from scipy import stats
 
 from reV.handlers.geotiff import Geotiff
 from reV.handlers.outputs import Outputs
@@ -33,9 +34,8 @@ class SupplyCurvePointSummary(SupplyCurvePoint):
     def __init__(self, gid, f_excl, f_gen, f_techmap, tm_dset_gen, tm_dset_res,
                  res_class_dset=None, res_class_bin=None, ex_area=0.0081,
                  power_density=None, dset_cf='cf_mean-means',
-                 dset_lcoe='lcoe_fcr-means', data_layers=None, resolution=64,
-                 exclusion_shape=None,
-                 close=False):
+                 dset_lcoe='lcoe_fcr-means', resolution=64,
+                 exclusion_shape=None, close=False):
         """
         Parameters
         ----------
@@ -74,10 +74,6 @@ class SupplyCurvePointSummary(SupplyCurvePoint):
         dset_lcoe : str | np.ndarray
             Dataset name from f_gen containing LCOE mean values.
             Can be pre-extracted generation output data in np.ndarray.
-        data_layers : None | dict
-            Aggregation data layers. Must be a dictionary keyed by data label
-            name. Each value must be another dictionary with "band", "method",
-            and "fpath".
         resolution : int | None
             SC resolution, must be input in combination with gid.
         exclusion_shape : tuple
@@ -91,7 +87,6 @@ class SupplyCurvePointSummary(SupplyCurvePoint):
         self._res_class_bin = res_class_bin
         self._dset_cf = dset_cf
         self._dset_lcoe = dset_lcoe
-        self._data_layers = data_layers
         self._res_gid_set = None
         self._gen_gid_set = None
         self._mean_res = None
@@ -376,6 +371,45 @@ class SupplyCurvePointSummary(SupplyCurvePoint):
             capacity = self.area * self.power_density
         return capacity
 
+    def agg_data_layers(self, summary, data_layers):
+        """Perform additional data layer aggregation.
+
+        Parameters
+        ----------
+        summary : dict
+            Dictionary of summary outputs for this sc point.
+        data_layers : None | dict
+            Aggregation data layers. Must be a dictionary keyed by data label
+            name. Each value must be another dictionary with "band", "method",
+            and "fpath".
+
+        Returns
+        -------
+        summary : dict
+            Dictionary of summary outputs for this sc point. A new entry for
+            each data layer is added.
+        """
+
+        if data_layers is not None:
+            for name, attrs in data_layers.items():
+
+                if 'fobj' not in attrs:
+                    with Geotiff(attrs['fpath']) as f:
+                        data = f[int(attrs['band']), self.rows, self.cols]
+                else:
+                    data = attrs['fobj'][int(attrs['band']),
+                                         self.rows, self.cols]
+
+                data = data[self.mask]
+                if attrs['method'] == 'mode':
+                    data = stats.mode(data)[0][0]
+                else:
+                    data = data.mean()
+
+                summary[name] = data
+
+        return summary
+
     @classmethod
     def summary(cls, gid, fpath_excl, fpath_gen, fpath_techmap, dset_tm,
                 gen_index, args=None, data_layers=None, **kwargs):
@@ -416,7 +450,7 @@ class SupplyCurvePointSummary(SupplyCurvePoint):
         """
 
         with cls(gid, fpath_excl, fpath_gen, fpath_techmap, dset_tm, gen_index,
-                 data_layers=data_layers, **kwargs) as point:
+                 **kwargs) as point:
 
             ARGS = {'res_gids': point.res_gid_set,
                     'gen_gids': point.gen_gid_set,
@@ -441,7 +475,134 @@ class SupplyCurvePointSummary(SupplyCurvePoint):
                     warn('Cannot find "{}" as an available SC point summary '
                          'output', OutputWarning)
 
+            summary = point.agg_data_layers(summary, data_layers)
+
         return summary
+
+
+class AggFileHandler:
+    """Simple framework to handle aggregation file context managers."""
+
+    def __init__(self, fpath_excl, fpath_gen, fpath_techmap, data_layers):
+        """
+        Parameters
+        ----------
+        fpath_excl : str
+            Filepath to exclusions geotiff.
+        fpath_gen : str
+            Filepath to .h5 reV generation output results.
+        fpath_techmap : str
+            Filepath to tech mapping between exclusions and generation results
+            The tech mapping module will be run if this file does not exist.
+        data_layers : None | dict
+            Aggregation data layers. Must be a dictionary keyed by data label
+            name. Each value must be another dictionary with "band", "method",
+            and "fpath".
+        """
+
+        self._excl = ExclusionPoints(fpath_excl)
+        self._gen = Outputs(fpath_gen, mode='r')
+        self._techmap = h5py.File(fpath_techmap, 'r')
+        self._data_layers = self._open_data_layers(data_layers)
+
+        # pre-initialize any import attributes
+        _ = self._gen.meta
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.close()
+        if type is not None:
+            raise
+
+    @staticmethod
+    def _open_data_layers(data_layers):
+        """Open data layer geotiff handlers.
+
+        Parameters
+        ----------
+        data_layers : None | dict
+            Aggregation data layers. Must be a dictionary keyed by data label
+            name. Each value must be another dictionary with "band", "method",
+            and "fpath".
+
+        Returns
+        -------
+        data_layers : None | dict
+            Aggregation data layers. fobj is added to the dictionary of each
+            layer.
+        """
+
+        if data_layers is not None:
+            for name, attrs in data_layers.items():
+                data_layers[name]['fobj'] = Geotiff(attrs['fpath'])
+        return data_layers
+
+    @staticmethod
+    def _close_data_layers(data_layers):
+        """Open data layer geotiff handlers.
+
+        Parameters
+        ----------
+        data_layers : None | dict
+            Aggregation data layers. Must have fobj geotiff handlers to close.
+        """
+
+        if data_layers is not None:
+            for name in data_layers.keys():
+                data_layers[name]['fobj'].close()
+
+    def close(self):
+        """Close all file handlers."""
+        self._excl.close()
+        self._gen.close()
+        self._techmap.close()
+        self._close_data_layers(self._data_layers)
+
+    @property
+    def exclusions(self):
+        """Get the exclusions file handler object.
+
+        Returns
+        -------
+        _excl : ExclusionPoints
+            Exclusions geotiff handler object.
+        """
+        return self._excl
+
+    @property
+    def gen(self):
+        """Get the gen file handler object.
+
+        Returns
+        -------
+        _gen : Outputs
+            reV gen outputs handler object.
+        """
+        return self._gen
+
+    @property
+    def techmap(self):
+        """Get the techmap file handler object.
+
+        Returns
+        -------
+        _techmap : h5py.File
+            Techmap h5 file handler.
+        """
+        return self._techmap
+
+    @property
+    def data_layers(self):
+        """Get the data layers object.
+
+        Returns
+        -------
+        _data_layers : dict
+            Data layers namespace.
+        """
+        return self._data_layers
 
 
 class Aggregation:
@@ -450,8 +611,7 @@ class Aggregation:
     def __init__(self, fpath_excl, fpath_gen, fpath_techmap, dset_tm,
                  res_class_dset=None, res_class_bins=None,
                  dset_cf='cf_mean-means', dset_lcoe='lcoe_fcr-means',
-                 data_layers=None,
-                 resolution=64, gids=None, n_cores=None):
+                 data_layers=None, resolution=64, gids=None, n_cores=None):
         """
         Parameters
         ----------
@@ -652,8 +812,7 @@ class Aggregation:
     def _serial_summary(fpath_excl, fpath_gen, fpath_techmap, dset_tm,
                         gen_index, res_class_dset=None, res_class_bins=None,
                         dset_cf='cf_mean-means', dset_lcoe='lcoe_fcr-means',
-                        data_layers=None,
-                        resolution=64, gids=None, **kwargs):
+                        data_layers=None, resolution=64, gids=None, **kwargs):
         """Standalone method to create agg summary - can be parallelized.
 
         Parameters
@@ -705,53 +864,47 @@ class Aggregation:
         summary = []
 
         with SupplyCurveExtent(fpath_excl, resolution=resolution) as sc:
-
+            points = sc.points
             exclusion_shape = sc.exclusions.shape
-
             if gids is None:
                 gids = range(len(sc))
 
-            # pre-extract handlers so they are not repeatedly initialized
-            with ExclusionPoints(fpath_excl) as excl:
-                with Outputs(fpath_gen, mode='r') as gen:
-                    with h5py.File(fpath_techmap, 'r') as techmap:
+        # pre-extract handlers so they are not repeatedly initialized
+        with AggFileHandler(fpath_excl, fpath_gen, fpath_techmap,
+                            data_layers) as fhandlers:
 
-                        # pre-extract data before iteration
-                        _ = gen.meta
+            inputs = Aggregation._get_input_data(fhandlers.gen, fpath_gen,
+                                                 res_class_dset,
+                                                 res_class_bins,
+                                                 dset_cf,
+                                                 dset_lcoe)
 
-                        inputs = Aggregation._get_input_data(gen, fpath_gen,
-                                                             res_class_dset,
-                                                             res_class_bins,
-                                                             dset_cf,
-                                                             dset_lcoe)
+            for gid in gids:
+                for ri, res_bin in enumerate(inputs[1]):
+                    try:
+                        pointsum = SupplyCurvePointSummary.summary(
+                            gid, fhandlers.exclusions, fhandlers.gen,
+                            fhandlers.techmap,
+                            dset_tm, gen_index,
+                            res_class_dset=inputs[0],
+                            res_class_bin=res_bin,
+                            dset_cf=inputs[2],
+                            dset_lcoe=inputs[3],
+                            data_layers=fhandlers.data_layers,
+                            resolution=resolution,
+                            exclusion_shape=exclusion_shape,
+                            **kwargs)
 
-                        for gid in gids:
-                            for ri, res_bin in enumerate(inputs[1]):
-                                try:
-                                    pointsum = SupplyCurvePointSummary.summary(
-                                        gid, excl, gen, techmap,
-                                        dset_tm, gen_index,
-                                        res_class_dset=inputs[0],
-                                        res_class_bin=res_bin,
-                                        dset_cf=inputs[2],
-                                        dset_lcoe=inputs[3],
-                                        data_layers=data_layers,
-                                        resolution=resolution,
-                                        exclusion_shape=exclusion_shape,
-                                        **kwargs)
+                    except EmptySupplyCurvePointError:
+                        pass
 
-                                except EmptySupplyCurvePointError:
-                                    pass
+                    else:
+                        pointsum['sc_gid'] = gid
+                        pointsum['sc_row_ind'] = points.loc[gid, 'row_ind']
+                        pointsum['sc_col_ind'] = points.loc[gid, 'col_ind']
+                        pointsum['res_class'] = ri
 
-                                else:
-                                    pointsum['sc_gid'] = gid
-                                    pointsum['sc_row_ind'] = \
-                                        sc.points.loc[gid, 'row_ind']
-                                    pointsum['sc_col_ind'] = \
-                                        sc.points.loc[gid, 'col_ind']
-                                    pointsum['res_class'] = ri
-
-                                    summary.append(pointsum)
+                        summary.append(pointsum)
 
         return summary
 
