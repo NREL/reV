41d2ac0b58a3b4e97f31140c334e933b665d564a
Michael Rossol <michael.rossol@nrel.gov>
2019-07-15 16:07:59 -0600

    start rpm module

--- reV/utilities/exceptions.py
+++ reV/utilities/exceptions.py
@@ -8,77 +8,72 @@ class reVError(Exception):
     """
     Generic Error for reV
     """
-    pass
 
 
 class ConfigError(Exception):
     """
     Error for bad configuration inputs
     """
-    pass
 
 
 class FileInputError(Exception):
     """
     Error during input file checks.
     """
-    pass
 
 
 class ExecutionError(Exception):
     """
     Error for execution failure
     """
-    pass
 
 
 class PipelineError(Exception):
     """
     Error for pipeline execution failure
     """
-    pass
 
 
 class HandlerKeyError(Exception):
     """
     KeyError for Handlers
     """
-    pass
 
 
 class HandlerRuntimeError(Exception):
     """
     RuntimeError for Handlers
     """
-    pass
 
 
 class HandlerValueError(Exception):
     """
     ValueError for Handlers
     """
-    pass
 
 
 class ResourceError(Exception):
     """
     Error for poorly formatted resource.
     """
-    pass
 
 
 class SAMExecutionError(Exception):
     """
     Execution error for SAM simulations
     """
-    pass
+
+
+class RPMValueError(Exception):
+    """
+    ValueError for RPM Pipeline
+    """
 
 
 class SupplyCurveError(Exception):
     """
     Execution error for SAM simulations
     """
-    pass
 
 
 class EmptySupplyCurvePointError(SupplyCurveError):
@@ -92,7 +87,6 @@ class SupplyCurveInputError(SupplyCurveError):
     """
     Execution error for SAM simulations
     """
-    pass
 
 
 class OutputWarning(Warning):
@@ -106,32 +100,27 @@ class ExtrapolationWarning(Warning):
     """
     Warning for when value will be extrapolated
     """
-    pass
 
 
 class ConfigWarning(Warning):
     """
     Warning for unclear or default configuration inputs
     """
-    pass
 
 
 class SAMInputWarning(Warning):
     """
     Warning for bad SAM inputs
     """
-    pass
 
 
 class HandlerWarning(Warning):
     """
     Warning during .h5 handling
     """
-    pass
 
 
 class FileInputWarning(Warning):
     """
     Warning during input file checks.
     """
-    pass
