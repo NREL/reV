95da7bdc652585f539ddfa6b25ee1d7843a0d5d6
grantbuster <grant.buster@nrel.gov>
2019-08-16 10:19:18 -0600

    added pipeline cli flag for active monitoring or no monitoring. default is no active monitoring, which i think is preferred.

--- reV/pipeline/cli_pipeline.py
+++ reV/pipeline/cli_pipeline.py
@@ -13,17 +13,20 @@ from reV.pipeline.pipeline import Pipeline
               help='reV pipeline configuration json file.')
 @click.option('--cancel', is_flag=True,
               help='Flag to cancel all jobs associated with a given pipeline.')
+@click.option('--monitor', is_flag=True,
+              help='Flag to monitor pipeline jobs continuously. '
+              'Default is not to monitor (kick off jobs and exit).')
 @click.option('-v', '--verbose', is_flag=True,
               help='Flag to turn on debug logging. Default is not verbose.')
 @click.pass_context
-def from_config(ctx, config_file, cancel, verbose):
+def from_config(ctx, config_file, cancel, monitor, verbose):
     """Run reV pipeline from a config file."""
     verbose = any([verbose, ctx.obj['VERBOSE']])
 
     if cancel:
         Pipeline.cancel_all(config_file)
     else:
-        Pipeline.run(config_file)
+        Pipeline.run(config_file, monitor=monitor)
 
 
 if __name__ == '__main__':
