12b102a664c3c273998b9174ca4cb345fb20ee41
grantbuster <grant.buster@nrel.gov>
2019-10-16 14:20:21 -0600

    split out lcoe and orca tests into seperate test files. ORCA still works with most recent master but Jake says its is outdated since early 2019. lol.

--- tests/test_econ_lcoe.py
+++ tests/test_econ_lcoe.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # pylint: skip-file
 """
-PyTest file for PV generation in Rhode Island.
+PyTest file for lcoe econ run in Rhode Island.
 
 Created on Thu Nov 29 09:54:51 2018
 
@@ -79,42 +79,6 @@ def test_fout(year):
         os.remove(fpath)
 
     assert result
-
-
-@pytest.mark.parametrize('rut_id', range(5))
-def test_ORCA(rut_id):
-    """Test LCOE results from subset of Rutgers wind analysis."""
-
-    # results from Rutgers analysis for each ensemble ID
-    baseline = {0: [160.1777458, 159.5480359, 59.725, 59.384, 59.161],
-                1: [160.7288996, 160.0425542, 59.822, 59.466, 59.213],
-                2: [159.9368503, 159.2512835, 59.595, 59.257, 59.066],
-                3: [155.3335381, 154.6299536, 57.831, 57.494, 57.402],
-                4: [155.9064394, 155.048778, 57.802, 57.439, 57.261],
-                }
-
-    points = TESTDATADIR + '/ORCA/rutgers_pp_slim.csv'
-    cf_file = TESTDATADIR + '/gen_out/rut_{}_node00_x000.h5'.format(rut_id)
-    sam_files = {'6MW_offshore': TESTDATADIR + '/ORCA/6MW_offshore.json',
-                 # 't200_t186': TESTDATADIR + '/ORCA/t200_t186.json',
-                 't233_t217': TESTDATADIR + '/ORCA/t233_t217.json',
-                 # 't325_t302': TESTDATADIR + '/ORCA/t325_t302.json',
-                 }
-    site_data = TESTDATADIR + '/ORCA/orca_site_data.csv'
-
-    obj = Econ.reV_run(points=points, sam_files=sam_files, cf_file=cf_file,
-                       cf_year=None, site_data=site_data,
-                       output_request='lcoe_fcr', n_workers=1,
-                       sites_per_split=25, points_range=None,
-                       fout=None, dirout=None, return_obj=True)
-
-    lcoe = list(obj.out['lcoe_fcr'])
-
-    msg = ('LCOE does not match (new, baseline): \n{} \n{}'
-           .format(lcoe, baseline[rut_id]))
-    result = np.allclose(lcoe, baseline[rut_id], rtol=RTOL, atol=ATOL)
-
-    assert result, msg
 
 
 def execute_pytest(capture='all', flags='-rapP'):
