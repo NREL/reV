2bccb9a8395da339c7d84ac12c14e7b47bbd8d2a
Michael Rossol <mrossol@gmail.com>
2019-11-14 11:02:01 -0700

    ExclusionsMask -> ExclusionMask

--- reV/supply_curve/__init__.py
+++ reV/supply_curve/__init__.py
@@ -3,6 +3,6 @@
 reV Supply Curve
 """
 from .aggregation import Aggregation
-from .exclusions import ExclusionsMask
+from .exclusions import ExclusionMask
 from .point_summary import SupplyCurvePointSummary
 from .supply_curve import SupplyCurve
