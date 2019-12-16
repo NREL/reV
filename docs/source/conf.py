cc47d4cc396d7d90e45d0488b89b5315077dc8d7
Michael Rossol <mrossol@gmail.com>
2019-12-16 10:02:28 -0700

    formatting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,7 +58,7 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 source_suffix = ['.rst', '.md']
-#source_suffix = '.rst'
+# source_suffix = '.rst'
 
 # source_parsers = {'.md': MarkdownParser}
 
