02108c1e5557e23d65a167cbd0ed8bc683faea3e
Michael Rossol <mrossol@gmail.com>
2020-01-28 14:23:53 -0700

    Add 3.8 as SAM is not compatible

--- setup.py
+++ setup.py
@@ -92,6 +92,7 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     test_suite="tests",
     install_requires=["click", "h5py", "numpy", "pandas", "psutil",
