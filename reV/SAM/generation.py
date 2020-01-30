f53b3c24a5b76db089ab57d7e384e54a5bb26caa
grantbuster <grant.buster@nrel.gov>
2020-01-30 16:07:42 -0700

    fixed pysam2 default objects

--- reV/SAM/generation.py
+++ reV/SAM/generation.py
@@ -544,7 +544,7 @@ class PV(Solar):
                 DEFAULTSDIR,
                 'SAM/USA AZ Phoenix Sky Harbor Intl Ap (TMY3).csv')
             self._default = pysam_pv.default('PVWattsNone')
-            self._default.LocationAndResource.solar_resource_file = res_file
+            self._default.SolarResource.solar_resource_file = res_file
             self._default.execute()
 
         return self._default
@@ -609,7 +609,7 @@ class CSP(Solar):
                 DEFAULTSDIR,
                 'SAM/USA AZ Phoenix Sky Harbor Intl Ap (TMY3).csv')
             self._default = pysam_csp.default('MSPTSingleOwner')
-            self._default.LocationAndResource.solar_resource_file = res_file
+            self._default.SolarResource.solar_resource_file = res_file
             self._default.execute()
 
         return self._default
@@ -1038,7 +1038,7 @@ class Wind(Generation):
             res_file = os.path.join(
                 DEFAULTSDIR, 'SAM/WY Southern-Flat Lands.csv')
             self._default = pysam_wind.default('WindPowerNone')
-            self._default.WindResourceFile.wind_resource_filename = res_file
+            self._default.Resource .wind_resource_filename = res_file
             self._default.execute()
 
         return self._default
