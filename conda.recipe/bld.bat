936d87982d82e9d588a4f4fa6d310752bcf65e8a
Michael Rossol <mrossol@gmail.com>
2020-05-19 11:26:49 -0600

    add conda build files

---
+++ conda.recipe/bld.bat
@@ -0,0 +1,3 @@
+@echo off
+%PYTHON% setup.py install
+if errorlevel 1 exit 1
---
+++ conda_build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+ARRAY=( 3.7 3.8 )
+
+export CONDA_BLD_PATH=~/conda-bld
+
+for i in "${ARRAY[@]}"
+do
+	conda-build conda.recipe/ --python $i
+done
+
+# convert package to other platforms
+platforms=( osx-64 linux-64 win-64 )
+find $CONDA_BLD_PATH/ -name *.tar.bz2 | while read file
+do
+    echo $file
+    #conda convert --platform all $file  -o $HOME/conda-bld/
+    for platform in "${platforms[@]}"
+    do
+       conda convert --platform $platform $file  -o $CONDA_BLD_PATH/
+    done
+done
+
+# upload packages to conda
+find $CONDA_BLD_PATH/ -name *.tar.bz2 | while read file
+do
+    echo $file
+    anaconda -t $ANACONDA_TOKEN upload -u nrel $file
+done
+
+echo "Building and uploading conda package done!"
+conda build purge
