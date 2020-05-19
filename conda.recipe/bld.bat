e5b7caf666e1af0c4886c2229e40383074d7f206
Michael Rossol <mrossol@gmail.com>
2020-05-19 15:22:36 -0600

    add channel to conda-build

--- conda.recipe/bld.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-%PYTHON% setup.py install
-if errorlevel 1 exit 1
--- conda_build.sh
+++ conda_build.sh
@@ -2,33 +2,34 @@
 
 set -e
 
-ARRAY=( 3.7 3.8 )
+PKG_NAME=nrel-rev
+
+ARRAY=( 3.7 )
 
 export CONDA_BLD_PATH=~/conda-bld
 
 for i in "${ARRAY[@]}"
 do
-	conda-build conda.recipe/ --python $i
+	conda-build conda.recipe/ --python $i --channel=nrel
 done
 
-# convert package to other platforms
-platforms=( osx-64 linux-64 win-64 )
-find $CONDA_BLD_PATH/ -name *.tar.bz2 | while read file
-do
-    echo $file
-    #conda convert --platform all $file  -o $HOME/conda-bld/
-    for platform in "${platforms[@]}"
-    do
-       conda convert --platform $platform $file  -o $CONDA_BLD_PATH/
-    done
-done
+# # convert package to other platforms
+# platforms=( osx-64 linux-64 win-64 )
+# find $CONDA_BLD_PATH/ -name $PKG_NAME*.tar.bz2 | while read file
+# do
+#     echo $file
+#     for platform in "${platforms[@]}"
+#     do
+#        conda convert --platform $platform $file  -o $CONDA_BLD_PATH/
+#     done
+# done
 
-# upload packages to conda
-find $CONDA_BLD_PATH/ -name *.tar.bz2 | while read file
-do
-    echo $file
-    anaconda upload -u nrel $file
-done
+# # upload packages to conda
+# find $CONDA_BLD_PATH/ -name $PKG_NAME*.tar.bz2 | while read file
+# do
+#     echo $file
+#     anaconda upload -u nrel $file
+# done
 
-echo "Building and uploading conda package done!"
-conda build purge
+# echo "Building and uploading conda package done!"
+# rm -rf $CONDA_BLD_PATH/*
