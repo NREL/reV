afdbd81739776d95d9ae938c2f3c8fc19edfa4d3
Michael Rossol <mrossol@gmail.com>
2020-05-20 10:38:07 -0600

    cleanup conda_build

--- conda_build.sh
+++ conda_build.sh
@@ -4,32 +4,29 @@ set -e
 
 PKG_NAME=nrel-rev
 
-ARRAY=( 3.7 )
+PY_VERSION=( 3.7 )
 
 export CONDA_BLD_PATH=~/conda-bld
 
-for i in "${ARRAY[@]}"
-do
-	conda-build conda.recipe/ --python $i --channel=nrel
-done
-
-# # convert package to other platforms
 platforms=( osx-64 linux-64 win-64 )
-find $CONDA_BLD_PATH/ -name $PKG_NAME*.tar.bz2 | while read file
+for py in "${PY_VERSION[@]}"
 do
-    echo $file
+	conda build conda.recipe/ --python=$py --channel=nrel
+    file=$(conda build conda.recipe/ --python=$py --output)
+    echo Building $file
     for platform in "${platforms[@]}"
     do
-       conda convert --platform $platform $file  -o $CONDA_BLD_PATH/
+       conda convert --platform $platform $file -o $CONDA_BLD_PATH/
     done
 done
 
 # upload packages to conda
 find $CONDA_BLD_PATH/ -name $PKG_NAME*.tar.bz2 | while read file
 do
-    echo $file
+    echo Uploading $file
     anaconda upload -u nrel $file
 done
 
 echo "Building and uploading conda package done!"
 rm -rf $CONDA_BLD_PATH/*
+ls $CONDA_BLD_PATH
