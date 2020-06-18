20ca824dd4b5f28a1f028cb94acf3a3f0ba51900
Michael Rossol <mrossol@gmail.com>
2020-06-18 14:20:18 -0600

    update conda build

--- README.rst
+++ README.rst
@@ -17,9 +17,6 @@ reV
 .. image:: https://anaconda.org/nrel/nrel-rev/badges/version.svg
     :target: https://anaconda.org/nrel/nrel-rev
 
-.. image:: https://anaconda.org/nrel/nrel-rev/badges/platforms.svg
-    :target: https://anaconda.org/nrel/nrel-rev
-
 .. image:: https://anaconda.org/nrel/nrel-rev/badges/license.svg
     :target: https://anaconda.org/nrel/nrel-rev
 
--- conda_build.sh
+++ conda_build.sh
@@ -4,25 +4,11 @@ set -e
 
 PKG_NAME=nrel-rev
 
-PY_VERSION=( 3.7 3.8 )
-
 export CONDA_BLD_PATH=~/conda-bld
-platforms=( osx-64 linux-64 win-64 )
-for py in "${PY_VERSION[@]}"
-do
-    conda build conda.recipe/ --python=$py --channel=nrel --channel=mrossol
-    file=$(conda build conda.recipe/ --python=$py --channel=nrel --channel=mrossol --output)
-    for platform in "${platforms[@]}"
-    do
-       conda convert --platform $platform $file -o $CONDA_BLD_PATH/
-    done
-done
 
-# upload packages to conda
-find $CONDA_BLD_PATH/ -name $PKG_NAME*.tar.bz2 | while read file
-do
-    anaconda upload -u nrel $file
-done
+conda build conda.recipe/ --channel=nrel
+
+anaconda upload -u nrel $(conda build conda.recipe/ --channel=nrel --output)
 
 echo "Building and uploading conda package done!"
 rm -rf $CONDA_BLD_PATH/*
