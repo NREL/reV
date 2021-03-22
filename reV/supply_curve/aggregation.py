fb057e15419f8abca02148608e152682c4e0e0ee
grantbuster <grant.buster@nrel.gov>
2021-03-22 12:08:51 -0600

    added check layers flag for pre-extracted exclusions

--- reV/supply_curve/aggregation.py
+++ reV/supply_curve/aggregation.py
@@ -273,7 +273,7 @@ class AbstractAggregation(ABC):
         logger.info('Pre-extracting full exclusion mask, this could take '
                     'up to 30min for a large exclusion config...')
         with ExclusionMaskFromDict(excl_fpath, layers_dict=excl_dict,
-                                   min_area=min_area,
+                                   check_layers=True, min_area=min_area,
                                    kernel=area_filter_kernel) as f:
             inclusion_mask = f.mask
             tm_mask = f._excl_h5[tm_dset] == -1
