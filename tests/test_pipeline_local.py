e09646a041b28c15ef4124d143455dd10836fc6a
Michael Rossol <mrossol@gmail.com>
2021-04-22 11:49:02 -0600

    fix shutil.copytree verions issue

--- tests/test_pipeline_local.py
+++ tests/test_pipeline_local.py
@@ -21,10 +21,10 @@ def test_pipeline_local():
     """Test the reV pipeline execution on a local machine."""
     with tempfile.TemporaryDirectory() as TEMP_DIR:
         pipeline_dir = os.path.join(TESTDATADIR, 'pipeline/')
-        shutil.copytree(pipeline_dir, TEMP_DIR, dirs_exist_ok=True)
-
-        out_dir = os.path.join(TEMP_DIR, 'outputs/')
-        fpipeline = os.path.join(TEMP_DIR, 'config_pipeline.json')
+        pipeline_dir = shutil.copytree(pipeline_dir,
+                                       os.path.join(TEMP_DIR, 'pipeline'))
+        out_dir = os.path.join(pipeline_dir, 'outputs/')
+        fpipeline = os.path.join(pipeline_dir, 'config_pipeline.json')
         fbaseline = os.path.join(pipeline_dir,
                                  'baseline_pipeline_multi-year.h5')
 
