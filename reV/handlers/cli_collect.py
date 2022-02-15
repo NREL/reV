30d44b7d0dc99f9d58c3c0d94be4830440b36bef
ppinchuk <ppinchuk@nrel.gov>
2022-02-15 16:12:48 -0700

    Remove name option from collect cli

--- reV/handlers/cli_collect.py
+++ reV/handlers/cli_collect.py
@@ -24,16 +24,13 @@ logger = logging.getLogger(__name__)
 
 @click.group()
 @click.version_option(version=__version__)
-@click.option('--name', '-n', default='reV_collect', type=str,
-              show_default=True,
-              help='Collection job name. Default is "reV_collect".')
 @click.option('-v', '--verbose', is_flag=True,
               help='Flag to turn on debug logging. Default is not verbose.')
 @click.pass_context
-def main(ctx, name, verbose):
+def main(ctx, verbose):
     """reV Collect Command Line Interface"""
     ctx.ensure_object(dict)
-    ctx.obj['NAME'] = name
+    ctx.obj['NAME'] = os.path.basename(os.getcwd())
     ctx.obj['VERBOSE'] = verbose
 
 
@@ -54,15 +51,12 @@ def valid_config_keys():
 @click.pass_context
 def from_config(ctx, config_file, verbose):
     """Run reV gen from a config file."""
-    name = ctx.obj['NAME']
 
     # Instantiate the config object
     config = CollectionConfig(config_file)
 
-    # take name from config if not default
-    if config.name.lower() != 'rev':
-        name = config.name
-        ctx.obj['NAME'] = name
+    # take name from config
+    name = ctx.obj['NAME'] = config.name
 
     # Enforce verbosity if logging level is specified in the config
     if config.log_level == logging.DEBUG:
