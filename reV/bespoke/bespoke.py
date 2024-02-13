dee901d81a247e27af569bd7bde0d497e218bb31
grantbuster <grant.buster@nrel.gov>
2024-02-13 11:28:41 -0700

    bug fix taking LCOE input kwargs from several prioritized sources

--- reV/bespoke/bespoke.py
+++ reV/bespoke/bespoke.py
@@ -969,26 +969,35 @@ class BespokeSinglePlant:
             updated based on the bespoke optimized system_capacity, includes
             fixed_charge_rate, system_capacity (kW), capital_cost ($),
             fixed_operating_cos ($), variable_operating_cost ($/kWh)
+            Data source priority: outputs, plant_optimizer,
+            original_sam_sys_inputs, meta
         """
 
-        if 'system_capacity' not in self.outputs:
-            msg = ('Could not find system_capacity in the outputs, need to '
-                   'run_plant_optimization() to get the optimized '
-                   'system_capacity before calculating LCOE!')
-            logger.error(msg)
-            raise RuntimeError(msg)
+        kwargs_list = ['fixed_charge_rate', 'system_capacity', 'capital_cost',
+                       'fixed_operating_cost', 'variable_operating_cost']
+        lcoe_kwargs = {}
 
-        lcoe_kwargs = {
-            'fixed_charge_rate':
-                self.original_sam_sys_inputs['fixed_charge_rate'],
-            'system_capacity': self.plant_optimizer.capacity,
-            'capital_cost': self.plant_optimizer.capital_cost,
-            'fixed_operating_cost': self.plant_optimizer.fixed_operating_cost,
-            'variable_operating_cost':
-                self.plant_optimizer.variable_operating_cost}
+        for kwarg in kwargs_list:
+            if kwarg in self.outputs:
+                lcoe_kwargs[kwarg] = self.outputs[kwarg]
+            elif getattr(self.plant_optimizer, kwarg, None) is not None:
+                lcoe_kwargs[kwarg] = getattr(self.plant_optimizer, kwarg)
+            elif kwarg in self.original_sam_sys_inputs:
+                lcoe_kwargs[kwarg] = self.original_sam_sys_inputs[kwarg]
+            elif kwarg in self.meta:
+                value = float(self.meta[kwarg].values[0])
+                lcoe_kwargs[kwarg] = value
 
         for k, v in lcoe_kwargs.items():
             self._meta[k] = v
+
+        missing = [k for k in kwargs_list if k not in lcoe_kwargs]
+        if any(missing):
+            msg = ('Could not find these LCOE kwargs in outputs, '
+                   'plant_optimizer, original_sam_sys_inputs, or meta: {}'
+                   .format(missing))
+            logger.error(msg)
+            raise KeyError(msg)
 
         return lcoe_kwargs
 
