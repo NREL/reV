e31055ab801173b6f77c74e28b567fff9b571a56
ppinchuk <ppinchuk@nrel.gov>
2024-06-07 15:03:18 -0600

    New default output spec

--- reV/supply_curve/points.py
+++ reV/supply_curve/points.py
@@ -2229,16 +2229,13 @@ class GenerationSupplyCurvePoint(AggregationSupplyCurvePoint):
             SupplyCurveField.MEAN_LCOE: self.mean_lcoe,
             SupplyCurveField.MEAN_RES: self.mean_res,
             SupplyCurveField.AREA_SQ_KM: self.area,
+            SupplyCurveField.CAPACITY_AC_MW: (
+                self.capacity if self.capacity_ac is None else self.capacity_ac
+            )
             SupplyCurveField.CAPACITY_DC_MW: self.capacity_dc,
         }
 
-        if self.capacity_ac is None:
-            ARGS[SupplyCurveField.CAPACITY_AC_MW] = self.capacity
-        else:
-            ARGS[SupplyCurveField.CAPACITY_AC_MW] = self.capacity_ac
-
         extra_atts = {
-            # SupplyCurveField.CAPACITY_AC: self.capacity_ac,
             SupplyCurveField.OFFSHORE: self.offshore,
             SupplyCurveField.SC_POINT_CAPITAL_COST: self.sc_point_capital_cost,
             SupplyCurveField.SC_POINT_FIXED_OPERATING_COST: (
