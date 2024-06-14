eb0b1cd58a1f0113cfc8cd2f97855c8c2b1a8f6b
ppinchuk <ppinchuk@nrel.gov>
2024-06-13 18:01:49 -0600

    Fix recalc test

--- tests/test_supply_curve_sc_aggregation.py
+++ tests/test_supply_curve_sc_aggregation.py
@@ -588,8 +588,12 @@ def test_recalc_lcoe(cap_cost_scale):
                                     * np.array(annual_cf).mean()
                                     * 8760,
                                     data["variable_operating_cost"])
-    assert np.allclose(summary[SupplyCurveField.MEAN_LCOE],
-                       expected_recalc_lcoe)
+    if cap_cost_scale == "1":
+        assert np.allclose(summary[SupplyCurveField.MEAN_LCOE],
+                           expected_recalc_lcoe)
+    else:
+        assert not np.allclose(summary[SupplyCurveField.MEAN_LCOE],
+                               expected_recalc_lcoe)
 
     fcr = summary[SupplyCurveField.FIXED_CHARGE_RATE]
     cap_cost = (summary[SupplyCurveField.COST_SITE_OCC_USD_PER_AC_MW]
