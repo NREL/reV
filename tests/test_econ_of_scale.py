Fix units + tests

tests/test_econ_of_scale.py
@@ -205,7 +205,7 @@ def test_econ_of_scale_baseline(request_h5):
             capital_cost_per_mw = SupplyCurveField.COST_SITE_OCC_USD_PER_AC_MW
             assert np.allclose(sc_df['mean_capital_cost'],
                                sc_df[capital_cost_per_mw]
-                               * data["system_capacity"])
+                               * data["system_capacity"] / 1000)
         assert np.allclose(sc_df[SupplyCurveField.COST_BASE_OCC_USD_PER_AC_MW],
                            sc_df[SupplyCurveField.COST_SITE_OCC_USD_PER_AC_MW])
 

tests/test_supply_curve_sc_aggregation.py
@@ -606,9 +606,9 @@ def test_recalc_lcoe(cap_cost_scale):
            * summary[SupplyCurveField.CAPACITY_AC_MW])
     voc = (summary[SupplyCurveField.COST_SITE_VOC_USD_PER_AC_MW]
            * summary[SupplyCurveField.CAPACITY_AC_MW])
-    aep = summary[SupplyCurveField.SC_POINT_ANNUAL_ENERGY_MWH] * 1000
+    aep_kwh = summary[SupplyCurveField.SC_POINT_ANNUAL_ENERGY_MWH] * 1000
 
-    lcoe = lcoe_fcr(fcr, cap_cost, foc, aep, voc)
+    lcoe = lcoe_fcr(fcr, cap_cost, foc, aep_kwh, voc)
     assert np.allclose(lcoe, summary[SupplyCurveField.MEAN_LCOE])
 
     cap_cost = (summary[SupplyCurveField.COST_BASE_OCC_USD_PER_AC_MW]
@@ -620,7 +620,7 @@ def test_recalc_lcoe(cap_cost_scale):
     voc = (summary[SupplyCurveField.COST_BASE_VOC_USD_PER_AC_MW]
            * summary[SupplyCurveField.CAPACITY_AC_MW])
 
-    lcoe = lcoe_fcr(fcr, cap_cost, foc, aep, voc)
+    lcoe = lcoe_fcr(fcr, cap_cost, foc, aep_kwh, voc)
     assert np.allclose(lcoe, summary[SupplyCurveField.MEAN_LCOE])
 
 